bc5c454fac252cd5b0959fe0d68f9250999a4557
Alejandro <alejandrolujan1999@outlook.es>
2025-02-25 01:28:51 +0000

    endpoints v.0.1

--- src/app.py
+++ src/app.py
@@ -8,7 +8,8 @@ from flask_swagger import swagger
 from flask_cors import CORS
 from utils import APIException, generate_sitemap
 from admin import setup_admin
-from models import db, User
+from models import db, User, People, Planet
+
 #from models import Person
 
 app = Flask(__name__)
@@ -49,3 +50,41 @@ def handle_hello():
 if __name__ == '__main__':
     PORT = int(os.environ.get('PORT', 3000))
     app.run(host='0.0.0.0', port=PORT, debug=False)
+
+#[GET] /people Listar todos los registros de people en la base de datos.
+@app.route('/people', methods=['GET'])
+def people_get():
+    response_body = {}
+    character = db.session.execute(db.select(People)).scalars()
+    response_body = {'characters': character.serialize()}
+    return response_body, 200
+
+#[GET] /people/<int:people_id> Muestra la información de un solo personaje según su id.
+@app.route('/people/<int:character_id>', methods=['GET'])
+def people_get_id(character_id):
+    response_body = {}
+    character = db.session.execute(db.select(People).filter(People.id == character_id)).scalar()
+    response_body['character'] = character.serialize()
+    return response_body, 201
+
+#[GET] /planets Listar todos los registros de planets en la base de datos.
+@app.route('/planets', methods=['GET'])
+def planet_get():
+    response_body = {}
+    planet = db.session.execute(db.select(Planet)).scalars()
+    response_body = {'planets': planet.serialize()}
+    return response_body, 202
+
+#[GET] /planets/<int:planet_id> Muestra la información de un solo planeta según su id.
+@app.route('/planets/<int:planet_id>', methods=['GET'])
+def planet_get_id(planet_id):
+    response_body = {}
+    planet = db.session.execute(db.select(Planet).filter(Planet.id == planet_id)).scalar()
+    response_body['planet'] = planet.serialize()
+    return response_body, 203
+
+#[GET] /users Listar todos los usuarios del blog.
+@app.route('/users', methods=['GET'])
+def get_users():
+    users = User.query.all()
+    return jsonify([user.serialize() for user in users]), 200
--- src/models.py
+++ src/models.py
@@ -1,6 +1,6 @@
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import String, Boolean
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy import String, Boolean, Integer, ForeignKey
+from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 db = SQLAlchemy()
 
@@ -9,11 +9,70 @@ class User(db.Model):
     email: Mapped[str] = mapped_column(String(120), unique=True, nullable=False)
     password: Mapped[str] = mapped_column(nullable=False)
     is_active: Mapped[bool] = mapped_column(Boolean(), nullable=False)
-
+    favorites = relationship("Favorite", back_populates="user", cascade="all, delete-orphan")
 
     def serialize(self):
         return {
             "id": self.id,
-            "email": self.email,
-            # do not serialize the password, its a security breach
+            "email": self.email
+        }
+
+class People(db.Model):
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column(String(120), nullable=False)
+    height: Mapped[str] = mapped_column(String(10))
+    mass: Mapped[str] = mapped_column(String(10))
+    hair_color: Mapped[str] = mapped_column(String(50))
+    skin_color: Mapped[str] = mapped_column(String(50))
+    eye_color: Mapped[str] = mapped_column(String(50))
+    birth_year: Mapped[str] = mapped_column(String(20))
+    gender: Mapped[str] = mapped_column(String(20))
+    planet_id: Mapped[int] = mapped_column(ForeignKey("planet.id"))
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "height": self.height,
+            "mass": self.mass,
+            "hair_color": self.hair_color,
+            "skin_color": self.skin_color,
+            "eye_color": self.eye_color,
+            "birth_year": self.birth_year,
+            "gender": self.gender,
+            "planet_id": self.planet_id
+        }
+
+class Planet(db.Model):
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column(String(120), nullable=False)
+    climate: Mapped[str] = mapped_column(String(50))
+    terrain: Mapped[str] = mapped_column(String(50))
+    population: Mapped[int] = mapped_column(Integer)
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "climate": self.climate,
+            "terrain": self.terrain,
+            "population": self.population
+        }
+
+class Favorite(db.Model):
+    id: Mapped[int] = mapped_column(primary_key=True)
+    user_id: Mapped[int] = mapped_column(ForeignKey("user.id"))
+    people_id: Mapped[int] = mapped_column(ForeignKey("people.id"), nullable=True)
+    planet_id: Mapped[int] = mapped_column(ForeignKey("planet.id"), nullable=True)
+    vehicle_id: Mapped[int] = mapped_column(ForeignKey("vehicle.id"), nullable=True)
+    
+    user = relationship("User", back_populates="favorites")
+    
+    def serialize(self):
+        return {
+            "id": self.id,
+            "user_id": self.user_id,
+            "people_id": self.people_id,
+            "planet_id": self.planet_id,
+            "vehicle_id": self.vehicle_id
         }
